22e35f0ee6a4c1300912378ed07d65b38f6f9b88
dskim0220 <kimds1842@gmail.com>
2026-01-12 15:35:14 +0900

    Update practice.py
    anaconda connect

--- practice.py
+++ practice.py
@@ -1,1 +1,9 @@
-print('hello World')
+import pandas as pd
+
+# 간단한 데이터프레임 만들기
+data = {'이름': ['A', 'B', 'C'], '램 용량': ['16GB', '32GB', '256GB']}
+df = pd.DataFrame(data)
+
+print("--- 아나콘다 연동 성공! ---")
+print(df)
+
